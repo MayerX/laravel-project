4315675c88746bab5ae4e5be140a47a34a002936
MayerX <meet_fate_x@icloud.com>
2022-01-14 03:45:03 +0800

    use Argon UI add import some css and js files into template

--- resources/views/common/app.blade.php
+++ resources/views/common/app.blade.php
@@ -5,9 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }}</title>
-    <link rel="stylesheet" href="{{ asset("css/bootstrap.min.css") }}">
-    <link rel="stylesheet" href="{{ asset("css/shards.min.css") }}">
+    <title>@yield('title', 'Default')</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{ asset("vendor/nucleo/css/nucleo.css") }}">
+    <link rel="stylesheet" href="{{ asset("vendor/@fortawesome/fontawesome-free/css/all.min.css") }}">
+    <!-- Argon CSS -->
+    <link rel="stylesheet" href="{{ asset("css/argon.min.css") }}">
     @yield('link')
 </head>
 <body>
@@ -15,9 +20,11 @@
     @yield('content')
     <footer>@yield('footer')</footer>
 
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/jquery-3.6.0.js') }}"></script>
-    <script src="{{ asset('js/shards.min.js') }}"></script>
+    <!-- Core -->
+    <script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Argon JS -->
+    <script src="{{ asset('js/argon.min.js') }}"></script>
     @yield('script')
 </body>
 </html>
